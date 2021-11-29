1a7ef109a276d6ad16baebd8c88c3547736a83fc
jayPreak <68821643+jayPreak@users.noreply.github.com>
2021-11-29 10:04:43 +0530

    Add files via upload

---
+++ RoomBookingSystem/src/main/java/project/Select.java
@@ -0,0 +1,42 @@
+package project;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 91958
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Select {
+
+    /**
+     *
+     * @param query
+     * @return
+     */
+    public static ResultSet getData(String query)
+    {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try
+        {
+            con = ConnectionProvider.getCon();
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+            return rs;
+        
+        }
+        catch(Exception e)
+        {
+              JOptionPane.showMessageDialog(null, e);
+                return null;
+        }
+    }
+  
+}
